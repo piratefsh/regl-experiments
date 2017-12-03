bed92e11bd02fb4fde657a646cbf4b2305522298
Sher Minn Chong <sherminn.chong@gmail.com>
2017-12-03 17:44:46 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
+/*
+  tags: basic
+
+  <p> This example is a simple demonstration of how to use regl to draw a triangle. </p>
+ */
+
+// The default method exposed by the module wraps a canvas element
 const regl = require('regl')()
-const mouse = require('mouse-change')()
+const fragmentShader = require('./fragment.glsl');
+const vertexShader = require('./vertex.glsl');
 
-var fsh = require('./fragment.glsl');
-var vsh = require('./vertex.glsl');
+// This clears the color buffer to black and the depth buffer to 1
+regl.clear({
+  color: [0, 0, 0, 1],
+  depth: 1
+})
 
-const pixels = regl.texture()
+// In regl, draw operations are specified declaratively using. Each JSON
+// command is a complete description of all state. This removes the need to
+// .bind() things like buffers or shaders. All the boilerplate of setting up
+// and tearing down state is automated.
+regl({
 
-const drawFeedback = regl({
-  frag: fsh,
-  vert: vsh,
+  // In a draw call, we can pass the shader source code to regl
+  frag: fragmentShader,
+  vert: vertexShader,
   attributes: {
     position: [
-      -2, 0,
-      0, -2,
-      2, 2]
+      [-1, 0],
+      [0, -1],
+      [1, 1]
+    ]
   },
 
   uniforms: {
-    resolution: (context)=>[context.viewportWidth, context.viewportHeight],
-    texture: pixels,
-    mouse: ({pixelRatio, viewportHeight}) => [
-      mouse.x * pixelRatio,
-      viewportHeight - mouse.y * pixelRatio
-    ],
-    t: ({tick}) => 0.01 * tick
+    color: [1, 0, 0, 1]
   },
+
   count: 3
-})
-
-regl.frame(function () {
-  regl.clear({
-    color: [0, 0, 0, 1]
-  })
-
-  drawFeedback()
-
-  pixels({
-    copy: true
-  })
-})
-
+})()
