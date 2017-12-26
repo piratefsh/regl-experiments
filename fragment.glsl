4194a4d4a67d6c00747f8aa43c0dec84e0dddbd1
Sher Minn <sherminn.chong@gmail.com>
2017-12-26 16:00:01 +0800

    HSB to RGB implementation

--- fragment.glsl
+++ fragment.glsl
@@ -13,6 +13,14 @@ vec4 when_eq(vec4 x, vec4 y){
   return 1.0 - abs(sign(x - y));
 }
 
+vec3 hsb2rgb(vec3 c){
+  vec3 m = mod(c.x * 6. + vec3(0., 4., 2.), 6.);
+  vec3 val = abs(m - 3.) - 1.;
+  vec3 rgb = clamp(val, 0., 1.);
+
+  rgb = rgb * rgb * (3. - 2. * rgb);
+  return c.z * mix(vec3(1.), rgb, c.y);
+}
 void main () {
   vec2 location = (u_mouse - gl_FragCoord.xy) / u_resolution;
   gl_FragColor = vec4(1., location.x, location.y, 1.0);
@@ -20,25 +28,20 @@ void main () {
 
   vec2 pos = gl_FragCoord.xy/u_resolution;
 
-  vec3 colorA = vec3(1. * sin(u_time), 0, 0);
-  vec3 colorB = vec3(0, 0, 1. - sin(u_time));
+  // vec3 colorA = vec3(1. * sin(u_time), 0, 0);
+  // vec3 colorB = vec3(0, 0, 1. - sin(u_time));
 
   vec3 pct = vec3(pos.x);
-  // pct.r = smoothstep(0., 1., pos.x);
   // pct.r = ceil(sin(pos.x/2.)) + floor(sin(pos.x/2.));
-  float oscillations = 4.;
+  float oscillations = 1.;
   float scaleSin = 0.5;
-  float shiftSin = 0.5;
+  float shiftSin = mod(sin(u_time), 1.);
   float y = sin(pos.x*PI*oscillations) * scaleSin + shiftSin;
+  float scale = 0.125;
   pct.r  = y;
-  float scale = 0.1;
-  // pct.g = step(0.5, y);
   pct.g = ceil(pos.x / scale) * scale;
-  // pos.y = ceil(pos.x/2.);
-  pct.b = 0.5;
-  vec3 color = mix(colorA, colorB, pct);
+  // pct.b = 0.5;
   // color = mix(color, vec3(1., 0., 0.), plot(pos, pct.r));
-  color = mix(colorA, colorB, pct.g);
-  // color = mix(color, vec3(0., 0., 1.), plot(pos, pct.b));
-  gl_FragColor = vec4(color, 1.0);
+  vec3 color = vec3(pct.r, 1, 1);
+  gl_FragColor = vec4(hsb2rgb(color), 1.);
 }
