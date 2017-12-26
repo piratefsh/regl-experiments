b5a9ee1ffc066208a412729f1ed466cba184365d
Sher Minn <sherminn.chong@gmail.com>
2017-12-26 14:34:07 +0800

    Add ceil stepping

--- fragment.glsl
+++ fragment.glsl
@@ -1,21 +1,40 @@
 precision mediump float;
+#define PI 3.1415926535897932384626433832795
+
 uniform float u_time; // tick
 uniform vec2 u_resolution; // canvas size
 uniform vec2 u_mouse; // mouse position
-const float PI = 3.1415926535897932384626433832795;
+
 float plot(vec2 pos, float pct){
   return smoothstep(pct-0.005, pct, pos.y) - smoothstep(pct, pct+0.005, pos.y);
 }
+
+vec4 when_eq(vec4 x, vec4 y){
+  return 1.0 - abs(sign(x - y));
+}
+
 void main () {
   vec2 location = (u_mouse - gl_FragCoord.xy) / u_resolution;
   gl_FragColor = vec4(1., location.x, location.y, 1.0);
 
-  vec2 pos = gl_FragCoord.xy/u_resolution - vec2(0.5, 0.5);
-  float y = 0.5 * sin(pos.x * PI * 2.); pow(pos.x, mod(floor(u_time * 10.), 10.));
-  vec3 color = vec3(0);
 
-  float pct = plot(pos, y);
-  color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
+  vec2 pos = gl_FragCoord.xy/u_resolution;
 
+  vec3 colorA = vec3(1., 1., 1.);
+  vec3 colorB = vec3(0., 0., 0.);
+
+  vec3 pct = vec3(pos.x);
+  // pct.r = smoothstep(0., 1., pos.x);
+  // pct.r = ceil(sin(pos.x/2.)) + floor(sin(pos.x/2.));
+  float y = sin(pos.x*10.) * 0.5;
+  pct.r  = y;
+  float scale = 0.1;
+  pct.g = floor(pos.x / scale) * scale;
+  // pos.y = ceil(pos.x/2.);
+  // pct.b = pow(pos.x, 0.5);
+  vec3 color = mix(colorA, colorB, pct);
+  color = mix(color, vec3(1., 0., 0.), plot(pos, pct.r));
+  color = mix(color, vec3(0., 1.0, 0.), plot(pos, pct.g));
+  // color = mix(color, vec3(0., 0., 1.), plot(pos, pct.b));
   gl_FragColor = vec4(color, 1.0);
 }
