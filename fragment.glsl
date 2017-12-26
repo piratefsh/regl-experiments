293c2c5b8ca7859a17acef90b0c618cea449048d
Sher Minn Chong <sherminn.chong@gmail.com>
2017-12-26 10:16:57 +0800

    Update

--- fragment.glsl
+++ fragment.glsl
@@ -1,5 +1,21 @@
 precision mediump float;
-uniform vec4 color;
+uniform float u_time; // tick
+uniform vec2 u_resolution; // canvas size
+uniform vec2 u_mouse; // mouse position
+const float PI = 3.1415926535897932384626433832795;
+float plot(vec2 pos, float pct){
+  return smoothstep(pct-0.005, pct, pos.y) - smoothstep(pct, pct+0.005, pos.y);
+}
 void main () {
-  gl_FragColor = color;
+  vec2 location = (u_mouse - gl_FragCoord.xy) / u_resolution;
+  gl_FragColor = vec4(1., location.x, location.y, 1.0);
+
+  vec2 pos = gl_FragCoord.xy/u_resolution - vec2(0.5, 0.5);
+  float y = 0.5 * sin(pos.x * PI * 2.); pow(pos.x, mod(floor(u_time * 10.), 10.));
+  vec3 color = vec3(0);
+
+  float pct = plot(pos, y);
+  color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
+
+  gl_FragColor = vec4(color, 1.0);
 }
--- index.js
+++ index.js
@@ -6,8 +6,10 @@
 
 // The default method exposed by the module wraps a canvas element
 const regl = require('regl')()
+const mouse = require('mouse-change')()
 const fragmentShader = require('./fragment.glsl');
 const vertexShader = require('./vertex.glsl');
+const pixels = regl.texture()
 
 // This clears the color buffer to black and the depth buffer to 1
 regl.clear({
@@ -19,22 +21,36 @@ regl.clear({
 // command is a complete description of all state. This removes the need to
 // .bind() things like buffers or shaders. All the boilerplate of setting up
 // and tearing down state is automated.
-regl({
+const draw = regl({
 
   // In a draw call, we can pass the shader source code to regl
   frag: fragmentShader,
   vert: vertexShader,
   attributes: {
     position: [
-      [-1, 0],
-      [0, -1],
-      [1, 1]
+      [-5, 0],
+      [10, -5],
+      [1, 5]
     ]
   },
 
   uniforms: {
-    color: [1, 0, 0, 1]
+    u_texture: pixels,
+    u_resolution: (context)=>[context.viewportWidth, context.viewportHeight],
+    u_mouse: ({pixelRatio, viewportHeight}) => [
+      mouse.x * pixelRatio,
+      viewportHeight - mouse.y * pixelRatio
+    ],
+    u_time: ({tick}) => { return 0.01 * tick }
   },
 
   count: 3
-})()
+});
+
+regl.frame(({tick}) => {
+  regl.clear({
+    color: [0, 0, 0, 1]
+  })
+
+  draw()
+})
